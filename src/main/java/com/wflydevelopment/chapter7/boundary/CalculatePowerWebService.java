21147ff7d9dac25911e13ec50cdcf533e238cc65
Adriano <adriano.jesus2@fatec.sp.gov.br>
2016-02-13 17:23:59 -0200

    Addition a webservice SOAP

---
+++ src/main/java/com/wflydevelopment/chapter7/boundary/CalculatePowerWebService.java
@@ -0,0 +1,7 @@
+package com.wflydevelopment.chapter7.boundary;
+
+public class CalculatePowerWebService {
+	public double calculatePower(double base, double exponent) {
+		return Math.pow(base, exponent);
+	}
+}
